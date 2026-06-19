63b79a85c8a13be16b5b507863e60eedb9bddecc
mwoohoohoo <m.hookway@live.co.uk>
2026-06-19 09:36:55 +0200

    Updates pre-build

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  base: "/Seasonal-calendar/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
